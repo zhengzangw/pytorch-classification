fd845809bee0121f043854ac18400b6188b87805
zhengzangw <zhengzangw@163.com>
2021-05-31 21:06:06 +0800

    [auto] auto commit by script when pushed to server

---
+++ scripts/sync.sh
@@ -0,0 +1,8 @@
+set -e
+
+# auto commit
+git add -A
+git commit --allow-empty -m "[auto] auto commit by script when pushed to server"
+
+# push to server
+rsync -ravh --include '.env' --exclude-from='.gitignore' . $@
